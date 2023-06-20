5ad9ebbec2d50f692e763ba8b5de0e7eff9d5d22
E-gap <106596181+E-gap@users.noreply.github.com>
2023-06-20 22:05:28 +0300

    update

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const app = require("./app");
 
 require("dotenv").config();
 
-const { DB_HOST, PORT } = process.env;
+/* const { DB_HOST, PORT } = process.env; */
 
-console.log("dsssd");
+const DB_HOST =
+  "mongodb+srv://evhengap:11021985@cluster0.jb42kwc.mongodb.net/usersreader?retryWrites=true&w=majority";
+
+const PORT = 3001;
 
 mongoose
   .connect(DB_HOST)
