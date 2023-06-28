aeb04662a2481a3c95fdcfcd3c5fded282a81221
E-gap <106596181+E-gap@users.noreply.github.com>
2023-06-28 22:49:38 +0300

    update

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,5 @@
 const HttpError = require("./HttpError");
-const sendEmail = require("./sendEmail");
+
 const {
   addContactsSchema,
   patchContactsFavoriteSchema,
@@ -23,5 +23,4 @@ module.exports = {
   emailSchema,
   registerSchema,
   loginSchema,
-  sendEmail,
 };
